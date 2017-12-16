65777435a6046484a74b8faff20e0c6b24a01280
blumikon98766 <marcinzaraj@gmail.com>
2017-12-16 12:26:04 +0100

    inne zmiany w pliku hello

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 class Hello{
-	oublic static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args) {
+		System.out.println("Hello jajaja");
+	}
+	
+	public sttic void bar(){
+	System.out.println("jajaja");
 	}
 }
